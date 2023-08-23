615207cf28ee0bd3ec365b3e56e6d7524049ee5d
Mauro <mauromaio.dev@gmail.com>
2023-08-22 22:07:53 -0300

    v1 finalizada

---
+++ models/User.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
