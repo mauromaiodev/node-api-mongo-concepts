845a76246e5cf90a3f2e46b15802302c688d9a1b
Mauro <mauromaio.dev@gmail.com>
2023-08-23 12:07:18 -0300

    error handling com try catch, destructuring e secure key com .env

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,78 @@
+import dotenv from "dotenv";
 import express, { Request, Response } from "express";
-import mongoose from "mongoose";
-import User from "./models/User";
+import mongoose, { ConnectOptions } from "mongoose";
+import User, { UserType } from "./models/User";
+
+dotenv.config({ path: "./.env" });
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/users", async (request: Request, response: Response) => {
-  const users = await User.find();
-
-  return response.json(users);
+app.get("/users", async (_: Request, response: Response) => {
+  try {
+    const users = await User.find();
+    return response.json(users);
+  } catch (error) {
+    return response.status(500).json({ error: "Erro interno do servidor" });
+  }
 });
 
 app.post("/users", async (request: Request, response: Response) => {
-  const user = request.body;
+  const newUserInput = request.body as UserType;
 
-  const newUser = await User.create(user);
-
-  return response.json(newUser);
+  try {
+    const newUser = await User.create(newUserInput);
+    return response.status(201).json(newUser);
+  } catch (error) {
+    return response.status(400).json({ error: "Requisição inválida" });
+  }
 });
 
 app.put("/users/:id", async (request: Request, response: Response) => {
   const userId = request.params.id;
-  const updatedUser = request.body;
+  const updatedUserInput = request.body as Partial<UserType>;
 
-  const user = await User.findByIdAndUpdate(userId, updatedUser, { new: true });
+  try {
+    const updatedUser = await User.findByIdAndUpdate(userId, updatedUserInput, {
+      new: true,
+    });
 
-  if (!user) {
-    return response.status(404).json({ error: "Usuário não encontrado" });
+    if (!updatedUser) {
+      return response.status(404).json({ error: "Usuário não encontrado" });
+    }
+
+    return response.json(updatedUser);
+  } catch (error) {
+    return response.status(500).json({ error: "Erro interno do servidor" });
   }
-
-  return response.json(user);
 });
 
 app.delete("/users/:id", async (request: Request, response: Response) => {
   const userId = request.params.id;
 
-  const user = await User.findByIdAndDelete(userId);
+  try {
+    const deletedUser = await User.findByIdAndDelete(userId);
 
-  if (!user) {
-    return response.status(404).json({ error: "Usuário não encontrado" });
+    if (!deletedUser) {
+      return response.status(404).json({ error: "Usuário não encontrado" });
+    }
+
+    return response.status(204).send();
+  } catch (error) {
+    return response.status(500).json({ error: "Erro interno do servidor" });
   }
-
-  return response.status(204).send();
 });
 
 mongoose
   .connect(
-    "mongodb+srv://mauromaiodev:4McGOTdr1dbDnW2m@cluster0.yamv0ox.mongodb.net/?retryWrites=true&w=majority"
+    process.env.MONGO_URI as string,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions
   )
   .then(() => console.log("Banco de dados conectado!"))
-  .catch(() => console.log("Não conectou"));
+  .catch((error) => console.log("Erro ao conectar ao banco de dados:", error));
 
 app.listen(3000);
