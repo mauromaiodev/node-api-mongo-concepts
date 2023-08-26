402a827363b0b973145a8720ad51e8ba3c655c72
Mauro <mauromaio.dev@gmail.com>
2023-08-26 01:44:22 -0300

    feat: bookModels

---
+++ src/models/Book.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+export interface BookType extends mongoose.Document {
+  title: string;
+  author: string;
+  rentBy: mongoose.Types.ObjectId | null;
+}
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    rentBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+const Book = mongoose.model<BookType>("Book", bookSchema);
+
+export default Book;
