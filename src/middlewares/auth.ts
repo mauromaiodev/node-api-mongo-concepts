1d0c847ae6cecc0325263a2cf1c23a4c48a46f07
Mauro <mauromaio.dev@gmail.com>
2023-08-26 01:44:54 -0300

    feat: auth userId

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -11,7 +11,7 @@ interface DecodedToken extends JwtPayload {
 declare global {
   namespace Express {
     interface Request {
-      user?: UserType;
+      user?: UserType & { userId: string };
     }
   }
 }
@@ -24,20 +24,21 @@ const authenticate = async (
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ message: "Authentication required" });
+    return res.status(401).json({ message: "Autenticação necessária." });
   }
 
   try {
     const decodedToken = jwt.verify(token, SECRET_KEY) as DecodedToken;
     const user = await User.findById(decodedToken.userId);
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(404).json({ message: "Usuário não encontrado." });
     }
 
     req.user = user;
+    req.user.userId = decodedToken.userId;
     next();
   } catch (error) {
-    res.status(401).json({ message: "Invalid token" });
+    res.status(401).json({ message: "Token inválida." });
   }
 };
 
