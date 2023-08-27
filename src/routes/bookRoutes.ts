ad251c85c6f41b299a2642dcabe8370f59977d20
Mauro <mauromaio.dev@gmail.com>
2023-08-27 19:26:49 -0300

    feat: update em bookRoutes

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,21 +1,32 @@
 import express from "express";
-import bookController from "../controllers/bookController";
+import * as bookController from "../controllers/bookController";
+import { BookInputDTO } from "../dtos/BookDTO";
 import authenticate from "../middlewares/auth";
 
 const router = express.Router();
 
 router.get("/", authenticate, bookController.getAllBooks);
-
 router.get("/rented", authenticate, bookController.getRentedBooks);
-
-router.post("/:bookId/rent", authenticate, bookController.rentBook);
-
-router.post("/:bookId/return", authenticate, bookController.returnBook);
-
-router.post("/", authenticate, bookController.createBook);
-
-router.put("/:bookId", authenticate, bookController.updateBook);
-
-router.delete("/:bookId", authenticate, bookController.deleteBook);
+router.post("/", authenticate, (req, res) => {
+  const newBookData: BookInputDTO = req.body;
+  return bookController.createBook(newBookData, res);
+});
+router.post("/:bookId/rent", authenticate, (req, res) => {
+  const { bookId } = req.params;
+  const { userId } = req.user!;
+  return bookController.rentBook(bookId, userId, res);
+});
+router.post("/:bookId/return", authenticate, (req, res) => {
+  const { bookId } = req.params;
+  return bookController.returnBook(bookId, res);
+});
+router.put("/:bookId", authenticate, (req, res) => {
+  const { bookId } = req.params;
+  return bookController.updateBook(bookId, req.body, res);
+});
+router.delete("/:bookId", authenticate, (req, res) => {
+  const { bookId } = req.params;
+  return bookController.deleteBook(bookId, res);
+});
 
 export default router;
